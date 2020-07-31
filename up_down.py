7477f8eeecb9989c62eb96ca83d2305ab968d06d
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-07-31 20:00:25 +0900

    2020-07-31

---
+++ up_down.py
@@ -0,0 +1,6 @@
+i = 10
+for i in range(1, 12):
+    for j in range(0, 13):
+        {}
+
+print(i, j)
---
+++ 심심해서_궁금한거_다시_코딩/pop_algo.py
@@ -0,0 +1,12 @@
+stack = []
+print(stack)
+stack.append(1)
+print(stack)
+stack.append(2)
+print(stack)
+stack.append(3)
+print(stack)
+
+for i in range(0 ,3):
+    stack.pop()
+    print(stack)
